7d814e46aa6668269d16054a1947fff57bd108f3
r-southworth <rachael.southworth@datagrove.com>
2023-03-16 17:56:09 -0400

    fix logo add supabase

---
+++ src/head_foot_data.ts
@@ -0,0 +1,12 @@
+export const headerData = {
+    links: [
+      {
+        text: 'Datagrove Home',
+        href: 'https://datagrove.com/',
+        
+      },
+    ],
+    actions: [
+      
+    ],
+  };
