e9786798db240dd519d189b80ab5e59ca6a4a3ff
jim <jimh@datagrove.com>
2023-03-25 08:13:23 -0400

    a

--- src/lib/pay.tsx
+++ src/lib/pay.tsx
@@ -1,6 +1,6 @@
 import { Show, createSignal, onMount } from 'solid-js'
 import { loadStripe } from '@stripe/stripe-js'
-import { Elements } from 'solid-stripe'
+import { CardCvc, CardExpiry, CardNumber, Elements, useStripe, useStripeElements } from 'solid-stripe'
 
 const api_key = import.meta.env.PUBLIC_STRIPE_API
 export function PayForm() {
@@ -15,9 +15,42 @@ export function PayForm() {
     <div><div>API key ={api_key}=</div>
     <Show when={stripe()} fallback={<div>Loading stripe</div>}>
       <Elements stripe={stripe() as any}>
-        {/* this is where your Stripe components go */}
+       <CheckoutForm/>
       </Elements>
     </Show>
     </div>
+  )
+}
+
+
+function CheckoutForm() {
+  const stripe = useStripe()
+  const elements = useStripeElements()
+
+  // const [, { Form }] = createRouteAction(async () => {
+  //   const clientSecret = "" // fetch from /api/create-payment-intent
+  //   // When the form submits, pass the CardNumber component to stripe().confirmCardPayment()
+  //   const result = await stripe().confirmCardPayment(clientSecret, {
+  //     payment_method: {
+  //       card: elements().getElement(CardNumber),
+  //       billing_details: {},
+  //     },
+  //   })
+
+  //   if (result.error) {
+  //     // payment failed
+  //   }
+  //   else {
+  //     // payment succeeded
+  //   }
+  // })
+
+  return (
+    <form>
+      <CardNumber />
+      <CardExpiry />
+      <CardCvc />
+      <button>Pay</button>
+    </form>
   )
 }
