76d96e12ec3829c36869530aca76e2bec6f4f9a7
r-southworth <rachael.southworth@datagrove.com>
2023-04-03 12:50:36 -0400

    Fully remove astro image for cloudflare compatibility

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import image from "@astrojs/image";
 import solidJs from "@astrojs/solid-js";
 
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image(), solidJs()],
+  integrations: [tailwind(), solidJs()],
   output: 'server',
   adapter: cloudflare()
 });
