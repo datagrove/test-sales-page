a0e2cfae25c7a265b0c03a46b5b3fc5cb7f42e74
r-southworth <rachael.southworth@datagrove.com>
2023-04-03 17:31:16 -0400

    try to switch to stripe checkout

---
+++ src/pages/checkout.ts
@@ -0,0 +1,35 @@
+
+import Stripe from 'stripe'
+import type { APIRoute } from 'astro';
+
+const stripe = new Stripe(import.meta.env.PRIVATE_STRIPE_API,{
+  apiVersion: '2022-11-15'
+})
+
+export const post: APIRoute = async function get ({params, request}:any) {
+
+  const body = await request.json()
+
+
+  const session = await stripe.checkout.sessions.create({
+    line_items: [
+      {
+        price: 'price_1MoBVGBRZLMDvS4RGtPKuq2k',
+        quantity: body.quantity,
+      },
+    ],
+    mode: 'payment',
+    success_url: `/thankyou`,
+    cancel_url: `/pay`,
+    automatic_tax: {enabled: true},
+  })
+
+  await get.redirect(303, session.url)
+
+  return new Response( JSON.stringify({clientSecret: paymentIntent.client_secret}),{
+    status: 200,
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
+}
