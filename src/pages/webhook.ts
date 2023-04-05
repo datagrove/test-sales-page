004eb79baf2c7a65e1862b9cd9d676e8656d4a63
r-southworth <rachael.southworth@datagrove.com>
2023-04-05 17:04:15 -0400

    try to add webhook

---
+++ src/pages/webhook.ts
@@ -0,0 +1,48 @@
+
+import Stripe from 'stripe'
+import type { APIRoute } from 'astro';
+
+const stripe = new Stripe(import.meta.env.PRIVATE_STRIPE_API, {
+  apiVersion: '2022-11-15'
+})
+
+const endpointSecret = import.meta.env.PRIVATE_STRIPE_ENDPOINT
+
+export const post: APIRoute = async function get({ params, request }: any) {
+
+  const body = await request.json()
+
+  const sig = request.headers['stripe-signature']
+
+  let event;
+
+  try {
+    event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret)
+  } catch (err:any) {
+    return new Response(
+      JSON.stringify({
+        message: `Webhook Error: ${err.message}`,
+      }),
+      { status: 400 }
+    );
+  }
+
+  switch (event.type){
+    case 'checkout.session.completed':
+      const session:any = event.data.object;
+      if (session.payment_status === 'paid'){
+        console.log(session)
+      }
+      break;
+
+    default:
+      console.log(`Unhandled event type ${event.type}`)
+  }
+
+  return new Response(
+    JSON.stringify({
+      message: `Success`,
+    }),
+    { status: 200 }
+  );
+}
