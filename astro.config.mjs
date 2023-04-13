6f63c52fa0c900dd54660b0ca5a02536edb71ccb
r-southworth <rachael.southworth@datagrove.com>
2023-04-13 12:12:13 -0400

    add vite define to astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,5 +8,15 @@ import cloudflare from "@astrojs/cloudflare";
 export default defineConfig({
   integrations: [tailwind(), solidJs()],
   output: 'server',
-  adapter: cloudflare({mode: 'directory'})
+  adapter: cloudflare({mode: 'directory'}),
+
+  vite: {
+    define: {
+      'process.env.API_URL': JSON.stringify(process.env.API_URL),
+      'process.env.anon_key': JSON.stringify(process.env.anon_key),
+      'process.env.PUBLIC_STRIPE_API': JSON.stringify(process.env.PUBLIC_STRIPE_API),
+      'process.env.PRIVATE_STRIPE_API': JSON.stringify(process.env.PRIVATE_STRIPE_API),
+      'process.env.PRIVATE_STRIPE_ENDPOINT': JSON.stringify(process.env.PRIVATE_STRIPE_ENDPOINT),
+    }
+  }  
 });
