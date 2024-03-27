e4493071c4b9f174a89feb262aabab82b6ab6052
Meagan <meagan.h.smith@gmail.com>
2024-03-27 14:38:28 -0500

    Add FAQ questions

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
+const plugin = require('tailwindcss/plugin')
+
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
@@ -12,6 +15,24 @@ module.exports = {
 	variants: {
 		fill: ['disabled', 'hover', 'focus'],
 	},
-	plugins: [],
+	plugins: [
+		plugin(function ({ addBase, addComponents, addUtilities, theme }) {
+			addComponents({
+				'.faq': {
+					fontSize: "24px",
+					margin: "4px 0px 4px 0px",
+					fontWeight: "bold",
+
+				},
+				'.answer': {
+					fontSize: "16px",
+					margin: "0px 0px 16px 0px"
+				},
+				'.faq-block': {
+					margin: "0px 0px 32px 0px"
+				}
+			})
+		})
+	],
 	darkMode: 'class',
 }
